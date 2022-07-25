5e98aec7c8c6b85b93a1f214ea07e1a8485b9f84
Vanessa <104469671+vanessa-vasconcelos@users.noreply.github.com>
2022-07-25 18:10:23 -0300

    Seção 3 finalizada

--- Exercicios/exer10.js
+++ Exercicios/exer10.js
@@ -1,3 +1,9 @@
-function ePaisagem(largura, altura) {
-    if 
-}
+// function ePaisagem(largura, altura) {
+//     return largura > altura ? true : false;
+
+// }
+// console.log(ePaisagem(10, 8))
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(1080, 1985));
---
+++ Exercicios/exer11.js
@@ -0,0 +1,13 @@
+const fizzBuzz = (num) => {
+    if(typeof num !== 'number') return num
+    if( num % 3 === 0 && num % 5 === 0) return "FizzBuzz"
+    if( num % 3 === 0) return 'Fizz';
+    if( num % 5 === 0) return "Buzz";
+    return num
+    
+}
+
+// console.log('a', fizzBuzz('a'))
+for (let i = 0; i<= 100; i++) {
+    console.log(i, fizzBuzz(i))
+}
