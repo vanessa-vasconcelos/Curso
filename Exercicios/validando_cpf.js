9b8d520db2eeec8e5736e5ab02f6625006f0ce92
Vanessa <104469671+vanessa-vasconcelos@users.noreply.github.com>
2022-08-02 18:46:56 -0300

    Finalizando Seção 6

--- Exercicios/validando_cpf.js
+++ Exercicios/validando_cpf.js
@@ -1,8 +1,46 @@
 function ValidaCpf(cpfEnviado) {
-    let cpfLimpo = cpfEnviado.replace(/\D+/g, '');// tudo que não for numero, subtitua
+   Object.defineProperty(this, 'cpfLimpo', {
+    enumerable: true,
+    get: function() {
+        return cpfEnviado.replace(/\D+/g, ''); // tudo que não for numero, subtitua
+    }
+   });
 }
 
-const cpf = new ValidaCpf('705.484.450-52');
-cpf.valida();
+ValidaCpf.prototype.valida = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia() === true) return false;
 
-console.log(cpfLimpo);
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+
+    const cpfNovo = cpfParcial + digito1 + digito2
+    console.log(cpfNovo)
+
+    return cpfNovo === cpfLimpo;
+    
+}
+
+ValidaCpf.prototype.criaDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+    
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
+        regressivo--;
+        return ac;
+    }, 0);
+
+    const digito = 11 - (total % 11)
+    return digito > 9 ? '0' : String(digito);
+};
+
+ValidaCpf.prototype.isSequencia = function() {
+const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+return sequencia === this.cpfLimpo;
+}
+
+const cpf = new ValidaCpf('111.111.111-11');
+console.log(cpf.valida())
