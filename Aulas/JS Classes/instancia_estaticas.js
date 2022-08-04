c15c8330b50d628a35c23ba96d10ec273d839708
Vanessa <104469671+vanessa-vasconcelos@users.noreply.github.com>
2022-08-04 20:02:24 -0300

    inicio da seção 8

--- Aulas/JS Classes/instancia_estaticas.js
+++ Aulas/JS Classes/instancia_estaticas.js
@@ -3,7 +3,7 @@ class ControleRemoto {
         this.tv = tv;
         this.volume = 0;
     };
-
+    // Método de instância - pq la embaixo, preciso chamar ela com controle1.aumantarVolume()
     aumentarVolume() {
         this.volume +=2;
     }
@@ -11,4 +11,17 @@ class ControleRemoto {
     diminuirVolume() {
         this.volume +=2;
     }
-}
+
+    // Método estático - chama pela a classe ControleRemoto.trocaPilha()
+    static trocaPilha() {
+        console.log('Ok, irei trocar')
+    }
+};
+
+const controle1 = new ControleRemoto('LG');
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+console.log(controle1);
+
+ControleRemoto.trocaPilha()
---
+++ Aulas/JS assincrono/promises.js
@@ -0,0 +1,22 @@
+function rand(max, min) {
+    min *= 1000;
+    max *= 1000;
+    return Math.floor(Math.random() * (max - min) + min);
+};
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(msg);
+        }, tempo)
+    })
+}
+
+esperaAi('Frase 1', rand(1, 3))
+.then(resposta => {
+    console.log(resposta);
+    return esperaAi('Frase 2', rand(1, 3));
+}).then(resposta => {
+    console.log(resposta)
+})
+.catch() //then executa resolve e catch executa o reject
