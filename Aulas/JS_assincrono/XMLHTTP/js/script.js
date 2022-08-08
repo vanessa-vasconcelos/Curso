5b86b9217b72d0b8e96deb3f1b7f2444a732961a
Vanessa <104469671+vanessa-vasconcelos@users.noreply.github.com>
2022-08-08 17:11:50 -0300

    Inicio da seção 9

--- Aulas/JS_assincrono/XMLHTTP/js/script.js
+++ Aulas/JS_assincrono/XMLHTTP/js/script.js
@@ -1,53 +1,50 @@
-const request = obj => {  // essa função vai buscar as paginas P1,P2,P3
-  return new Promise((resolve, reject) => {
-    const  xhr = new XMLHttpRequest();
-    xhr.open(obj.method, obj.url, true); // true para assicrono
-    xhr.send();
-
-    xhr.addEventListener('load', () => {
+const request = obj => {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open(obj.method, obj.url, true);
+      xhr.send();
+  
+      xhr.addEventListener('load', () => {
         if(xhr.status >= 200 && xhr.status < 300) {
-            resolve(xhr.responseText); //retorna o texto recebido de um servidor após o envio de uma solicitação.
-        }  else {
-            reject(xhr.statusText)
+          resolve(xhr.responseText);
+        } else {
+          reject(xhr.statusText);
         }
-    })
-  })
-    
-}
-
-document.addEventListener('click', e => {
+      });
+    });
+  };
+  
+  document.addEventListener('click', e => {
     const el = e.target;
     const tag = el.tagName.toLowerCase();
-
-    if(tag === 'a') {
-        e.preventDefault();
-        carregaPagina(el);
+  
+    if (tag === 'a') {
+      e.preventDefault();
+      carregaPagina(el);
     }
-});
-
-async function carregaPagina(el) {
-    const href = el.getAttribute('href') // retorna o valor de um argumento específico do elemento.
-    
+  });
+  
+  async function carregaPagina(el) {
+    const href = el.getAttribute('href');
+  
     const objConfig = {
-        method: 'GET',
-        URL: href,
-        
-    }
-
+      method: 'GET',
+      url: href
+    };
+  
     try {
-    const response = await request(objConfig);
-    carregaResultado(response);
-    }catch(e) {
-        console.log(e)
+      const response = await request(objConfig);
+      carregaResultado(response);
+    } catch(e) {
+      console.log(e);
     }
-    
-}
-
-function carregaResultado (response) {
+  }
+  
+  function carregaResultado(response) {
     const resultado = document.querySelector('.resultado');
     resultado.innerHTML = response;
-}
-
+  }
+  
 
 // MODO ANTIGO
 // const request = obj => {  // essa função vai buscar as paginas P1,P2,P3
